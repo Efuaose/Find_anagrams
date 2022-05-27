7941441f532709afb3cb035598990ed8c7e3e1cd
Efuaose <83923854+Efuaose@users.noreply.github.com>
2022-05-27 11:50:48 +0100

    Update find_anagram.py

--- find_anagram.py
+++ find_anagram.py
@@ -1,14 +1,8 @@
-# Check if two words are anagrams 
-# Example:
-# find_anagrams("hello", "check") --> False
-# find_anagrams("below", "elbow") --> True
-
-
 from turtle import position
 
 
 def find_anagram(word, anagram):
-    # [assignment] Add your code here
+
     list1 = list(word)
     list2 = list(anagram)
 
